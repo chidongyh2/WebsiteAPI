e68b910423f0645a148c7db8699483d6a98c2d80
ngovanquy17091997 <ngovanquy17091997@gmail.com>
2019-05-17 17:25:21 +0700

    commit

---
+++ GHM.Infrastructure/Models/ApiThrowServiceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GHM.Infrastructure.Models
+{
+    public class ApiThrowServiceInfo
+    {
+        public string GrantType { get; set; }
+        public string ClientId { get; set; }
+        public string Scopes { get; set; }
+        public string ApiGatewayUrl { get; set; }
+        public string TenantId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
