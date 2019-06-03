2e1ad2e61ddb001099aee7fbe32d6f113c66f6b4
GHMSOFT <GHMSOFT@TGHMSOFT>
2019-06-03 16:49:37 +0700

    commit

--- GHM.Website.TTMedic/Controllers/HomeController.cs
+++ GHM.Website.TTMedic/Controllers/HomeController.cs
@@ -81,6 +81,8 @@ namespace GHM.Website.TTMedic.Controllers
             var Activity = await httpClientService.GetAsync<NewsDetailViewModel>($"{requestUrl.ApiGatewayUrl}/api/v1/website/news/client/{apiService.TenantId}/tuyen-ngon-hoat-dong/{CultureInfo.CurrentCulture.Name}");
             ViewBag.activity = Activity;
 
+            var listMenuProduct = await httpClientService.GetAsync<List<MenuItemViewModel>>($"{requestUrl.ApiGatewayUrl}/api/v1/website/menus/position/{(int)Position.Middle}/items/menu/{apiService.TenantId}/{CultureInfo.CurrentCulture.Name}");
+            ViewBag.MenuProduct = listMenuProduct;
             return View();
         }
 
