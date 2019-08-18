50b46805e55125d06f907495cf597495f585fb4e
Dinh Nam <dvnam1010@gmail.com>
2019-08-18 20:46:08 +0700

    commit 18/8

--- GHM.WebSite.Nelly/wwwroot/js/home.es5.js
+++ GHM.WebSite.Nelly/wwwroot/js/home.es5.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
     });
 
     $("#middle-menu-slide").lightSlider({
-        item: 4,
+        item: 3,
         auto: false,
         loop: false,
         slideMove: 1,
@@ -28,8 +28,8 @@ $(document).ready(function () {
         slideMargin: 20,
         pauseOnHover: false,
         controls: true,
-        prevHtml: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-        nextHtml: '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+        //prevHtml: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+        //nextHtml: '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
         pager: false
     });
     $("#myCarousel").owlCarousel({
@@ -43,6 +43,7 @@ $(document).ready(function () {
         dotsEach: true,
         navigation: true,
         animateIn: true,
+        animateOut: 'fadeOut',
         autoplayTimeout: 5000
     });
 });
