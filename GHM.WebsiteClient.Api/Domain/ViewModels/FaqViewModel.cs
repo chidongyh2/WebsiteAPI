c1036ba54b6ee1aaf6902126a444d8f1a24fbde1
manhtv <manh.tv@ghmsoft.vn>
2019-09-23 18:10:54 +0700

    commit

---
+++ GHM.WebsiteClient.Api/Domain/ViewModels/FaqViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GHM.WebsiteClient.Api.Domain.ViewModels
+{
+    public class FaqViewModel
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
