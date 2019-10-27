109df949350ba54ad650f4ab3dacd8db778a3545
Dinh Nam <dvnam1010@gmail.com>
2019-10-27 19:08:25 +0700

    commit

--- GHM.WebSite.Nelly/Models/AgencyMeta.cs
+++ GHM.WebSite.Nelly/Models/AgencyMeta.cs
@@ -9,6 +9,14 @@ namespace GHM.WebSite.Nelly.Models
         [Display(Name = "Họ tên")]
         public string Name { get; set; }
 
-        
+        [StringLength(50, ErrorMessage = "Email không được nhỏ hơn 5 ký tự và lớn hơn 50 ký tự", MinimumLength = 5)]
+        [EmailAddress(ErrorMessage = "Đinh dạng email không đúng")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [StringLength(50, ErrorMessage = "Số điện thoại không được nhỏ hơn 5 ký tự và lớn hơn 50 ký tự", MinimumLength = 5)]
+        [Required(ErrorMessage = "Số điện thoại không được bỏ trống")]
+        [Display(Name = "Số điện thoại")]
+        public string PhoneNumber { get; set; }
     }
 }
--- GHM.WebSite.Nelly/Models/OrderMeta.cs
+++ GHM.WebSite.Nelly/Models/OrderMeta.cs
@@ -11,7 +11,6 @@ namespace GHM.WebSite.Nelly.Models
         public string FullName { get; set; }
 
         [StringLength(50, ErrorMessage = "Email không được nhỏ hơn 5 ký tự và lớn hơn 50 ký tự", MinimumLength = 5)]
-        [Required(ErrorMessage = "Emai không được để trống")]
         [EmailAddress(ErrorMessage = "Đinh dạng email không đúng")]
         [Display(Name = "Email")]
         public string Email { get; set; }
